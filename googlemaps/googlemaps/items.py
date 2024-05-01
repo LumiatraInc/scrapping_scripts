865b1a6214d6096351207fe96974c8e4d50339ba
Jaydrey <jaribwetshi64@gmail.com>
2024-05-01 21:08:14 +0300

    feat(items): created the google maps business item

--- googlemaps/googlemaps/items.py
+++ googlemaps/googlemaps/items.py
@@ -7,6 +7,24 @@ import scrapy
 
 
 class GooglemapsItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    business_name = scrapy.Field()
+    map_link = scrapy.Field()
+    search_term = scrapy.Field()
+    business_about = scrapy.Field()
+    business_type = scrapy.Field()
+    business_closed = scrapy.Field()
+    business_address = scrapy.Field()
+    business_hours = scrapy.Field()
+    phone_number = scrapy.Field()
+    website = scrapy.Field()
+    photos = scrapy.Field()
+    ratings = scrapy.Field()
+    total_ratings = scrapy.Field()
+    total_reviews = scrapy.Field()
+    reviews = scrapy.Field()
+    instagram_link = scrapy.Field()
+    x_link = scrapy.Field()
+    youtube_link = scrapy.Field()
+    facebook_link = scrapy.Field()
+    pinterest_link = scrapy.Field()
+
