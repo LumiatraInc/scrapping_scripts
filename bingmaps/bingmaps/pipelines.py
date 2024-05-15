becd4701b43b40b8ebdda5ae2d72bdae010edaaf
Jaydrey <jaribwetshi64@gmail.com>
2024-05-15 15:21:29 +0300

    feat(pipelines): cleaning name, type, about and phone number of businesses

--- bingmaps/bingmaps/pipelines.py
+++ bingmaps/bingmaps/pipelines.py
@@ -1,13 +1,33 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+from scrapy.exceptions import DropItem
 
 class BingmapsPipeline:
     def process_item(self, item, spider):
+        business = ItemAdapter(item)
+
+        for field, value in business.items():
+            if field == "business_name":
+                if value is None:
+                    raise DropItem(f"Dropped the business")
+                else:
+                    business[field] = value.strip()
+            
+            elif field == "business_type":
+                if value == ".":
+                    business[field] = ""
+                if value:
+                    business[field] = value.strip()
+            
+            elif field == "business_about":
+                if value is None:
+                    business[field] = ""
+                else:
+                    business[field] = value.strip()
+            
+            elif field == "phone_number":
+                if value is None:
+                    business[field] = ""
+                else:
+                    business[field] = value.replace("tel:", "").strip()
+            
         return item
