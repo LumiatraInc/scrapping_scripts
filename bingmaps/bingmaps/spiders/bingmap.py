ce77a3d283f12aa874de234c5d048f4102ac8339
Jaydrey <jaribwetshi64@gmail.com>
2024-05-15 15:24:07 +0300

    feat(spider): added try except clauses to catch errors and created function to get business type

--- bingmaps/bingmaps/spiders/bingmap.py
+++ bingmaps/bingmaps/spiders/bingmap.py
@@ -57,8 +57,17 @@ class BingmapSpider(scrapy.Spider):
                 print(f" We have {len(businesses_div)} businesses")
 
 
-                for index, business in enumerate(businesses_div):
+                for business in businesses_div:
                     yield self.parse_business(business)
+                    # close the business card
+                    close_business_card_btn = self.driver.find_element(By.CSS_SELECTOR, "div.taskCard.focus div.cardFace.front div.cardContent div.contentRoot div a.commonButton.backArrowButton.overlayButton")
+                    close_business_card_btn.click()
+                    # self.driver.back()
+                    WebDriverWait(self.driver, 10).until(
+                            lambda driver: driver.execute_script(
+                                "return document.readyState") == "complete"
+                    )
+
                     time.sleep(4)
 
                 time.sleep(2)
@@ -98,21 +107,20 @@ class BingmapSpider(scrapy.Spider):
 
 
     def parse_business(self, business: WebElement) -> BingmapsItem:
-
+        new_business = BingmapsItem()
+        business_name:str = None
+        business_type: str = None
+        socials: dict = {}
+        address: str = None
+        phone_number: str = None
+        website: str = None
+        business_status: str = None
+        rating: str = None
+        total_reviews: str = None
+        business_about: str = None
+        search_term = self.search_term
+        source = "Bing Maps"
         try:
-            new_business = BingmapsItem()
-
-            business_name:str = None
-            socials: dict = {}
-            address: str = None
-            phone_number: str = None
-            website: str = None
-            business_status: str = None
-            rating: str = None
-            total_reviews: str = None
-            business_about: str = None
-            search_term = self.search_term
-            source = "Bing Maps"
 
             if business.is_displayed() and business.is_enabled():
                 print(" ================> clicked on business")
@@ -133,6 +141,7 @@ class BingmapSpider(scrapy.Spider):
                     infos = self.parse_business_info(business_info_box)
                     business_name = infos.get("business_name")
                     business_about = infos.get("business_about")
+                    business_type = infos.get("business_type")
                     website = infos.get("website")
                     business_status = infos.get("business_status")
                     address = infos.get("address")
@@ -147,6 +156,7 @@ class BingmapSpider(scrapy.Spider):
 
             
             new_business["business_name"] = business_name
+            new_business["business_type"] = business_type
             new_business["business_about"] = business_about
             new_business["business_link"] = self.driver.current_url
             new_business["business_status"] = business_status
@@ -160,27 +170,21 @@ class BingmapSpider(scrapy.Spider):
             new_business["source"] = source
             new_business["search_term"] = search_term
 
-            print(f"new_business {new_business}")
+        except NoSuchElementException as ne:
+                print(f"Parse Business ========= An exception was thrown {ne}")
+            
 
-
-            # close the business card
-            close_business_card_btn = self.driver.find_element(By.CSS_SELECTOR, "div.taskCard.focus div.cardFace.front div.cardContent div.contentRoot div a.commonButton.backArrowButton.overlayButton")
-            close_business_card_btn.click()
-            # self.driver.back()
-            WebDriverWait(self.driver, 10).until(
-                    lambda driver: driver.execute_script(
-                        "return document.readyState") == "complete"
-            )
-
-            return new_business
-
+        except TimeoutException as te:
+            print(f"Parse Business ========= An exception was thrown {te}")
+        
 
         except Exception as e:
-            pass
+            print(f"Parse Business ========= An exception was thrown {e}")
+        
 
+        return new_business
     
     def parse_business_info(self, elements: SelectorList[Selector]) -> dict[str, (str | None)]:
-        print("=================> found business info box")
         print("step 1")
         business_name = self.get_business_name(elements)
 
@@ -199,10 +203,13 @@ class BingmapSpider(scrapy.Spider):
         print("step 6")
         business_about = self.get_business_about(elements)
 
+        print("step 7")
+        business_type = self.get_business_type(elements)
 
         return {
             "business_name": business_name,
             "business_about": business_about,
+            "business_type": business_type,
             "business_status": business_status,
             "address": address,
             "phone_number": phone_number,
@@ -211,84 +218,132 @@ class BingmapSpider(scrapy.Spider):
     
 
     def get_business_name(self, elements: SelectorList[Selector]) -> str | None:
-        business_name_el = elements.css("div.infoModule.b_divsec.bottomBleed.noSeparator div.b_annotate div.bm_annotationRoot h2.nameContainer")
-        if business_name_el:
-            return business_name_el.css("::text").get()
-        
-        return None
+        try:
+            business_name_el = elements.css("div.infoModule.b_divsec.bottomBleed.noSeparator div.b_annotate div.bm_annotationRoot h2.nameContainer")
+            if business_name_el:
+                return business_name_el.css("::text").get()
+            
+            return None
+        except Exception:
+            return None
+    
+    def get_business_type(self, elements: SelectorList[Selector]) -> str | None:
+        try:
+            # business_type_el = elements.css("div.infoModule.b_divsec.topBleed.noSeparator > div.b_factrow > a + text::text")
+            business_type_el = elements.xpath("//div/a/following-sibling::text()")
+            if business_type_el:
+                return business_type_el.get().replace(".", "").strip()
+            
+            business_type_el = elements.xpath("//div[contains(@class, 'infoModule'), contains(@class, 'b_divsec')]/div[contains(@class, 'b_factrow')]/text()")
+            if business_type_el:
+                return business_type_el.get().replace(".", "").strip()
+            return None
+        except Exception:
+            return None
+    
     
     def get_business_about(self, elements: SelectorList[Selector]) -> str | None:
-        about_el = elements.css("div#wire4 span")
-        if about_el:
-            return about_el.css("::attr(title)").get()
+        try:
+            about_el = elements.css("div.infoModule.b_divsec > div.ed_entity_desc > div.b_snippet_expansion > div.b_expandable_hidden_container.b_inline span")
+            if about_el:
+                return about_el.css("::attr(title)").get()
+            
+            # about_el = elements.css("div.infoModule.b_divsec > div.ed_entity_desc::text")
+            about_el = elements.xpath("//div[contains(@class, 'infoModule'), contains(@class, 'b_divsec')]/div[contains(@class, 'ed_entity_desc')]/text()")
+            if about_el:
+                return about_el.get()
+            return None
+        except Exception:
+            return None
 
     
     def get_business_status(self, elements: SelectorList[Selector]) -> str | None:
-        business_name_el = elements.css("div.infoModule.b_divsec.bottomBleed.noSeparator div.b_annotate div.bm_annotationRoot h2.nameContainer")
-        business_status_el = business_name_el.css("div.infobubble_item.isclaimedinfobubble_item")
-        if business_status_el:
-            return business_status_el.css("::attr(aria-label)").get()
-        
-        return None
+        try:
+            business_name_el = elements.css("div.infoModule.b_divsec.bottomBleed.noSeparator div.b_annotate div.bm_annotationRoot h2.nameContainer")
+            business_status_el = business_name_el.css("div.infobubble_item.isclaimedinfobubble_item")
+            if business_status_el:
+                return business_status_el.css("::attr(aria-label)").get()
+            
+            return None
+        except Exception:
+            return None
     
     def get_business_address(self, elements: SelectorList[Selector]) -> str | None:
-        address_el = elements.css("div[aria-label='Address'] > div.iconDataList::text")
-        if address_el:
-            return address_el.get()
-        
-        return None
+        try:
+            address_el = elements.css("div[aria-label='Address'] > div.iconDataList::text")
+            if address_el:
+                return address_el.get()
+            
+            return None
+        except Exception:
+            return None
 
     def get_business_phone_number(self, elements: SelectorList[Selector]) -> str | None:
-        phone_number_el = elements.css("div[aria-label='Phone'] > a::attr(href)")
-        if phone_number_el:
-            return phone_number_el.get()
-        
-        return None
+        try:
+            phone_number_el = elements.css("div[aria-label='Phone'] > a::attr(href)")
+            if phone_number_el:
+                return phone_number_el.get()
+            
+            return None
+        except Exception:
+            return None
     
     def get_business_website_link(self, elements: SelectorList[Selector]) -> str | None:
-        website_el = elements.css("div[aria-label='Website'] a")
-        if website_el:
-            return website_el.css("::attr(href)").get()
-        
-        return None
+        try:
+            website_el = elements.css("div[aria-label='Website'] a")
+            if website_el:
+                return website_el.css("::attr(href)").get()
+            
+            return None
+        except Exception:
+            return None
     
     def get_business_ratings(self, elements: SelectorList[Selector]) -> str | None:
-        rating_el = elements.css("div#slideexp0_B899C6c div.b_slidesContainer div.b_viewport.scrollbar div#slideexp0_B899C6 div.slide a.wr_rv div.wr_rat")
-        if rating_el:
-            return rating_el.css("::text").get()
-            
-        return None
+        try:
+            rating_el = elements.css("div#slideexp0_B899C6c div.b_slidesContainer div.b_viewport.scrollbar div#slideexp0_B899C6 div.slide a.wr_rv div.wr_rat")
+            if rating_el:
+                return rating_el.css("::text").get()
+                
+            return None
+        except Exception:
+            return None
     
     def get_total_business_reviews(self, elements: SelectorList[Selector]) -> str | None:
-        total_reviews_el = elements.css("div#slideexp0_B899C6c div.b_slidesContainer div.b_viewport.scrollbar div#slideexp0_B899C6 div.slide a.wr_rv div.wr_rev.b_demoteText")
-        if total_reviews_el:
-            return total_reviews_el.css("::text").get()
-            
-        return None
+        try:
+            total_reviews_el = elements.css("div#slideexp0_B899C6c div.b_slidesContainer div.b_viewport.scrollbar div#slideexp0_B899C6 div.slide a.wr_rv div.wr_rev.b_demoteText")
+            if total_reviews_el:
+                return total_reviews_el.css("::text").get()
+                
+            return None
+        except Exception:
+            return None
 
     def get_business_social_media(self, elements: SelectorList[Selector]) -> dict[str, str]:
-        socials = {}
-        social_media_el = elements.css("div[aria-label='Social profiles'] ul.b_hList li a")
-        if social_media_el:
-            for media in social_media_el:
-                if "facebook.com" in media.css("::attr(href)").get():
-                    socials["facebook"] = media.css("::attr(href)").get()
+        try:
+            socials = {}
+            social_media_el = elements.css("div[aria-label='Social profiles'] ul.b_hList li a")
+            if social_media_el:
+                for media in social_media_el:
+                    if "facebook.com" in media.css("::attr(href)").get():
+                        socials["facebook"] = media.css("::attr(href)").get()
 
-                elif "twitter.com" in media.css("::attr(href)").get():
-                    socials["twitter"] = media.css("::attr(href)").get()
+                    elif "twitter.com" in media.css("::attr(href)").get():
+                        socials["twitter"] = media.css("::attr(href)").get()
 
-                elif "instagram.com" in media.css("::attr(href)").get():
-                    socials["instagram"] = media.css("::attr(href)").get()
+                    elif "instagram.com" in media.css("::attr(href)").get():
+                        socials["instagram"] = media.css("::attr(href)").get()
 
-                elif "linkedin.com" in media.css("::attr(href)").get():
-                    socials["linkedin"] = media.css("::attr(href)").get()
+                    elif "linkedin.com" in media.css("::attr(href)").get():
+                        socials["linkedin"] = media.css("::attr(href)").get()
 
-                elif "youtube.com" in media.css("::attr(href)").get():
-                    socials["youtube"] = media.css("::attr(href)").get()
+                    elif "youtube.com" in media.css("::attr(href)").get():
+                        socials["youtube"] = media.css("::attr(href)").get()
 
-                else:
-                    print(media.css("::attr(href)").get())
+                    else:
+                        print(media.css("::attr(href)").get())
 
-        return socials
+            return socials
+        except Exception:
+            return {}
     
 
