bfbc9c2dc3e9db8167516373dec758d43087c27f
Jaydrey <jaribwetshi64@gmail.com>
2024-05-23 15:04:24 +0300

    feat(items): added the relevant fields to the twitter proifle item

--- twitter/twitter/items.py
+++ twitter/twitter/items.py
@@ -7,6 +7,17 @@ import scrapy
 
 
 class TwitterItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    profile_name = scrapy.Field()
+    profile_photo = scrapy.Field()
+    cover_photo = scrapy.Field()
+    profile_hashtag = scrapy.Field()
+    is_verified = scrapy.Field()
+    profile_description = scrapy.Field()
+    company_type = scrapy.Field()
+    web_link = scrapy.Field()
+    date_joined = scrapy.Field()
+    total_followers = scrapy.Field()
+    total_following = scrapy.Field()
+    total_posts = scrapy.Field()
+
+    
