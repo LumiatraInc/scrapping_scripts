c9126e70e2481a5f6c87a3fbe3791706d833a53e
Jaydrey <jaribwetshi64@gmail.com>
2024-05-23 15:04:53 +0300

    feat(pipelines): added the json writer pipeline to write data in json format

--- twitter/twitter/pipelines.py
+++ twitter/twitter/pipelines.py
@@ -5,9 +5,29 @@
 
 
 # useful for handling different item types with a single interface
+import os, json
 from itemadapter import ItemAdapter
 
 
 class TwitterPipeline:
     def process_item(self, item, spider):
         return item
+
+
+class JsonWriterPipeline:
+    def open_spider(self, spider):
+        filename = "twitter_profile.json"
+        self.file = open(filename, 'w')
+        self.file.write("[")
+
+    def process_item(self, item, spider):
+        line = json.dumps(dict(item)) + ",\n"
+        self.file.write(line)
+        
+        return item
+
+    def close_spider(self, spider):
+        self.file.seek(self.file.tell() - 2, os.SEEK_SET)
+        self.file.write("]\n")
+        self.file.close()
+
