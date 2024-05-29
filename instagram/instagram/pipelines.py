ef5e421cef324aec8dc08d1b2a4fc0d3c647d84c
Jaydrey <jaribwetshi64@gmail.com>
2024-05-29 17:02:45 +0300

    feat(pipeline): created the pipeline for converting items to json file

--- instagram/instagram/pipelines.py
+++ instagram/instagram/pipelines.py
@@ -1,13 +1,26 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
+import os, json
 from itemadapter import ItemAdapter
 
 
 class InstagramPipeline:
     def process_item(self, item, spider):
         return item
+
+
+class JsonWriterPipeline:
+    def open_spider(self, spider):
+        filename = "instagram_profile.json"
+        self.file = open(filename, 'w')
+        self.file.write("[")
+
+    def process_item(self, item, spider):
+        line = json.dumps(dict(item)) + ",\n"
+        self.file.write(line)
+        
+        return item
+
+    def close_spider(self, spider):
+        self.file.seek(self.file.tell() - 2, os.SEEK_SET)
+        self.file.write("]\n")
+        self.file.close()
+
